5f7dd86a8501b33d6e897182461058f3156ec755
Paul J. Davis <paul.joseph.davis@gmail.com>
2011-11-01 21:40:37 +0000

    First pass at script deb builds

---
+++ bigcouch/bin/build_deb.rb
@@ -0,0 +1,28 @@
+#!/usr/bin/ruby
+
+if ARGV.length != 2
+    puts "usage: #{$0} tag version"
+    exit 1
+end
+
+tag = ARGV[0]
+vsn = ARGV[1]
+
+url = "https://github.com/cloudant/bigcouch/tarball/#{tag}"
+appfile = "apps/couch/src/couch.app.src"
+
+build = <<-EOH
+rm -rf build/
+mkdir -p build/
+wget -q #{url} -O build/bigcouch_#{vsn}.orig.tar.gz
+tar -C build -zxf build/bigcouch_#{vsn}.orig.tar.gz
+mv build/cloudant-bigcouch-* build/bigcouch-#{vsn}
+cp -r debian build/bigcouch-#{vsn}/debian
+cd build/bigcouch-#{vsn}
+cat #{appfile} | sed s/%VSN%/#{vsn}/ > #{appfile}.tmp
+mv #{appfile}.tmp #{appfile}
+debuild -us -uc
+EOH
+
+puts "building bigcouch packages"
+system(build)
